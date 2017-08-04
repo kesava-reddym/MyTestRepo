66733d9736028a8fac1fe1244704d928ac44b8e4
kesava-reddym <kesava.reddym19@gmail.com>
2017-08-04 21:40:58 +0530

    Signed-off-by: kesava-reddym <kesava.reddym19@gmail.com>

---
+++ src/test/java/myPack/MyClass.java
@@ -0,0 +1,17 @@
+package myPack;
+
+import org.testng.annotations.Test;
+
+public class MyClass {
+	
+	
+	@Test
+	public void tc_001()
+	{
+		System.out.println("Welcome to TestNg..");
+	}
+	
+	
+	
+
+}
